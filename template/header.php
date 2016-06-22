880fb5690152b825c9d627386b228d0f4fb6c0d1
Denham B. Rogers <denhamrogers10@gmail.com>
2016-06-22 12:55:52 -0500

    Added other Apple Icons
    Added different sized apple icons for different devices.

--- template/header.php
+++ template/header.php
@@ -2,7 +2,11 @@
 <html>
 <title>Ag Data Works Survey</title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
-<link href="http://survey.agdataworks.com/img/Icon.png" rel="apple-touch-icon" />
+<link href="http://survey.agdataworks.com/img/apple-icons/Icon.png" rel="apple-touch-icon" />
+<link href="http://survey.agdataworks.com/img/apple-icons/Icon-76.png" rel="apple-touch-icon" sizes="76x76" />
+<link href="http://survey.agdataworks.com/img/apple-icons/Icon-Small-40@3x.png" rel="apple-touch-icon" sizes="120x120" />
+<link href="http://survey.agdataworks.com/img/apple-icons/Icon-76@2x.png" rel="apple-touch-icon" sizes="152x152" />
+<link href="http://survey.agdataworks.com/img/apple-icons/Icon-60@3x.png" rel="apple-touch-icon" sizes="180x180" />
 <body>
 <div class="wrapper">
 	<div class="header">
