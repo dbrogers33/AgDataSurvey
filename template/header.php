cb197cfabee696ad3b9c54ce6d874c0211f103af
Denham B. Rogers <denhamrogers10@gmail.com>
2016-06-22 13:22:26 -0500

    JS gets rid of mobile browser
    To make the survey perform more like a native app, I have added so JS
    that will hide the browser bar at the top and the bottom.

--- template/header.php
+++ template/header.php
@@ -2,11 +2,13 @@
 <html>
 <title>Ag Data Works Survey</title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<script type="text/javascript" src="js/hide-address-bar.js"></script>
 <link href="http://survey.agdataworks.com/img/apple-icons/Icon.png" rel="apple-touch-icon" />
 <link href="http://survey.agdataworks.com/img/apple-icons/Icon-76.png" rel="apple-touch-icon" sizes="76x76" />
 <link href="http://survey.agdataworks.com/img/apple-icons/Icon-Small-40@3x.png" rel="apple-touch-icon" sizes="120x120" />
 <link href="http://survey.agdataworks.com/img/apple-icons/Icon-76@2x.png" rel="apple-touch-icon" sizes="152x152" />
 <link href="http://survey.agdataworks.com/img/apple-icons/Icon-60@3x.png" rel="apple-touch-icon" sizes="180x180" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
 <body>
 <div class="wrapper">
 	<div class="header">
